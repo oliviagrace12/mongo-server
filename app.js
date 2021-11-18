d2e9ab06a99f95492a1712f9a6bc9df146567089
Olivia Chisman <olivia.chisman@gmail.com>
2021-11-17 23:36:40 -0600

    react build

--- app.js
+++ app.js
@@ -20,4 +20,10 @@ app.use('/auth', authRouter);
 app.use('/item', itemRouter);
 app.use('/theme', themeRouter);
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
 module.exports = app;
